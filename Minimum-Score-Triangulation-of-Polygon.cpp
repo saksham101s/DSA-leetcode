be78a6ef24d18840eccc90a23eaca930adc76b09
saksham upreti <160235809+saksham101s@users.noreply.github.com>
2025-09-29 12:45:16 +0530

    [1039] [Time Beats: 27.81%] [Memory Beats: 44.38%] - LeetPush

---
+++ Minimum-Score-Triangulation-of-Polygon.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minScoreTriangulation(vector<int>& values) {
+        int n = values.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        // gap = length of interval
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                dp[i][j] = INT_MAX;
+                for (int k = i + 1; k < j; k++) {
+                    int cost =
+                        dp[i][k] + dp[k][j] + values[i] * values[j] * values[k];
+                    dp[i][j] = min(dp[i][j], cost);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+};
